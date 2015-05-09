7d6933272048db7add4d286ef28f03fca627a2ce
Bayu Suwarning Hadi <bayusuwarninghadi@gmail.com>
2015-05-09 10:06:54 +0700

    Simplified

--- js/site.js
+++ js/site.js
@@ -1,16 +1,1 @@
-function initialize() {
-    var myLatlng = new google.maps.LatLng(-6.202382, 106.801041);
-    var mapOptions = {
-        zoom: 15,
-        center: myLatlng
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'Bayu Suwarning Hadi Was Here!'
-    });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+$(document).ready(function(){function initialize(){var myLatlng=new google.maps.LatLng(-6.202382,106.801041);var mapOptions={zoom:15,center:myLatlng};var map=new google.maps.Map(document.getElementById("map-canvas"),mapOptions);var marker=new google.maps.Marker({position:myLatlng,map:map,title:"Bayu Suwarning Hadi Was Here!"});}google.maps.event.addDomListener(window,"load",initialize);});
